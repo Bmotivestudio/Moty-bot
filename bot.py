43f0f42ec33909b1666a297308cd262a9a572eb8
Bmotivestudio <bmotivestudio@gmail.com>
2025-04-23 15:06:34 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup\
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, CallbackQueryHandler\
-\
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, CallbackQueryHandler
 # Inserisci il tuo token qui (non condividerlo mai pubblicamente!)\
 BOT_TOKEN = "8152725037:AAFPPLVStK6GwmU1qHR7QKq1X_msQHUg6Mo"
 \
