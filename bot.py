e87ee7a03f45c1f0c2309d53779e79d1ae02771c
Bmotivestudio <bmotivestudio@gmail.com>
2025-04-23 20:04:15 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,16 +2,15 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN ="8087961436:AAGlF0kYCl8Fn-UAr3OclgUCJdke5yMeKUg"
+TOKEN = os.getenv("8087961436:AAGlF0kYCl8Fn-UAr3OclgUCJdke5yMeKUg")
 WEBHOOK_BASE_URL = os.getenv("https://moty-bot.onrender.com")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Ciao! Sono Moty, l'assistente di Bmotive Studio!")
+    await update.message.reply_text("Ciao! Sono Moty, il tuo assistente AI.")
 
 def main():
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-
     app.run_webhook(
         listen="0.0.0.0",
         port=10000,
